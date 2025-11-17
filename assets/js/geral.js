5cf5f559d772f13b344814b32c75563e0bf2ba08
enzin7 <enzomota7@gmail.com>
2025-11-17 16:18:45 -0300

    Site em Desenvolvimento

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -3,6 +3,7 @@ aberto = false;
 function abrirSobre() {
   document.getElementById("sobre_rodape").style.display = "block";
   document.body.style.cursor = "pointer";
+  document.body.style.overflow = "hidden";
 
   setTimeout(() => {
     aberto = true;
@@ -13,6 +14,8 @@ document.body.addEventListener("click", () => {
   if (aberto) {
     document.getElementById("sobre_rodape").style.display = "none";
     document.body.style.cursor = "auto";
+    document.body.style.overflow = "visible";
+
     aberto = false;
   }
 });
