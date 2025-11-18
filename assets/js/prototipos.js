3a01a69493b411a1d317f8347b0893fc372300ca
enzin7 <enzomota7@gmail.com>
2025-11-18 16:07:49 -0300

    Site em Desenvolvimento

---
+++ assets/js/prototipos.js
@@ -0,0 +1,43 @@
+aberto = false;
+
+function abrirSobre() {
+  if (!aberto) {
+    document.getElementById("sobre_rodape").style.display = "block";
+    document.getElementById("lista-menu").style.display = "none";
+    document.body.style.cursor = "pointer";
+    document.body.style.overflow = "hidden";
+
+    setTimeout(() => {
+      aberto = true;
+    }, 500);
+  }
+}
+
+function abrirMenuHamb() {
+  if (!aberto) {
+    document.getElementById("lista-menu").style.display = "block";
+    document.getElementById("sobre_rodape").style.display = "none";
+    document.body.style.cursor = "pointer";
+    document.body.style.overflow = "hidden";
+
+    setTimeout(() => {
+      aberto = true;
+    }, 300);
+  } else if (aberto) {
+    document.getElementById("lista-menu").style.display = "none";
+
+    setTimeout(() => {
+      aberto = false;
+    }, 300);
+  }
+}
+
+document.body.addEventListener("click", () => {
+  if (aberto) {
+    document.getElementById("sobre_rodape").style.display = "none";
+    document.body.style.cursor = "auto";
+    document.body.style.overflow = "visible";
+
+    aberto = false;
+  }
+});
